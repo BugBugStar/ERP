bd4613ac14dc8e3811fe8c829d334a97bf334c63
008_100003703987_qq287095981 <008_100003703987_qq287095981@git.cloud.tencent.com>
2019-08-02 01:55:10 +0800

    引入ng-zorro-antd组件 customer使用antd的nz-table组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +10,11 @@ import { CreateOrderComponent } from './create-order/create-order.component';
 import { SalesSummaryComponent } from './sales-summary/sales-summary.component';
 import { CustomersComponent } from './customers/customers.component';
 import { RepositoryComponent } from './repository/repository.component';
+
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -20,7 +27,10 @@ import { RepositoryComponent } from './repository/repository.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    /** 导入 ng-zorro-antd 模块 **/
+    NgZorroAntdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
