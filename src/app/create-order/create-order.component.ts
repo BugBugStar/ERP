2229b0a6af0828c5e9c7a9b134c3d697bc570585
008_100003703987_qq287095981 <008_100003703987_qq287095981@git.cloud.tencent.com>
2019-08-08 23:34:40 +0800

    仓库、创建订单页面显示表格

--- src/app/create-order/create-order.component.ts
+++ src/app/create-order/create-order.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-create-order',
-  templateUrl: './create-order.component.html',
-  styleUrls: ['./create-order.component.css']
+    selector: 'app-create-order',
+    templateUrl: './create-order.component.html',
+    styleUrls: ['./create-order.component.css']
 })
 export class CreateOrderComponent implements OnInit {
+    elementKeys = ['id', 'name', 'customer', 'saler', 'sales_notes_no', 'place_date',];
+    // 'product', 'length', 'unit_price', 'amount', 'style_number', 'good_date', 'term'];
+    tableKey = 'create_order';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-repository',
-  templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.css']
+    selector: 'app-repository',
+    templateUrl: './repository.component.html',
+    styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
+    elementKeys = ['id', 'name', 'item_code', 'color', 'length', 'unit_price',];
+    tableKey = 'repository';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
