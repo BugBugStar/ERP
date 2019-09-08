27a7ea0dbbe14f0e7f54b846573cd8200e40a620
008_100003703987_qq287095981 <008_100003703987_qq287095981@git.cloud.tencent.com>
2019-09-08 22:51:49 +0800

    1、公司信息页面支持配公账银行卡号及支行名、私账银行卡号及支行名、地址 2、订单详情，含税时，银行户口不一样

--- src/app/company-base-info/company-base-info.component.ts
+++ src/app/company-base-info/company-base-info.component.ts
@@ -11,7 +11,24 @@ export class CompanyBaseInfoComponent implements OnInit {
         {
             name: 'id',
             hidden: true,
-        }, 'name', 'bank', 'account'];
+        }, 'name',
+        {
+            name: 'bank_of_public_account',
+            chineseName: '对公账户银行'
+        },
+        {
+            name: 'public_account',
+            chineseName: '对公账户'
+        },
+        {
+            name: 'bank_of_self_account',
+            chineseName: '个人账户银行'
+        },
+        {
+            name: 'self_account',
+            chineseName: '个人账户银行'
+        },
+    ];
     tableKey = 'company_base_info';
 
     constructor() { }
